f4a1d8628bb4460e7bd04107999c8b0fec7abedd
unknown <inathan.moreira.10@gmail.com>
2020-07-28 19:00:36 -0300

    Adding Application Backend

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Lindo');
+app.use(cors());
 
-    response.json([
-        'Inathan'
-    ]);
-});
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
